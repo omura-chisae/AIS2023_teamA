7f7f051f7de7dbd6f74174f24b7012d5a1689df8
yoshiki-0633 <yoshikikoizumi9703@gmail.com>
2023-11-02 14:45:01 +0900

    ページ遷移追加しました。

---
+++ src/pages/BottomTab.tsx
@@ -0,0 +1,18 @@
+import { memo } from "react";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { UserPage } from "./UserPage";
+import { SearchRecipes } from "./SearchRecipes";
+import { Stock } from "./Stock";
+import { Settings } from "./Settings";
+
+
+export const BottomTab = memo(() => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Stock" component={Stock} />
+      <Tab.Screen name="SearchRecipes" component={SearchRecipes} />
+      <Tab.Screen name= 'Settings' component={Settings}/>
+    </Tab.Navigator>
+  );
+})
---
+++ src/pages/Stock.tsx
@@ -0,0 +1,6 @@
+import { memo } from "react";
+
+export const Stock = memo(() => {
+  return <p>在庫管理画面入れてください。</p>;
+
+});
